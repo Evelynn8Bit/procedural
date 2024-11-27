992134c1ffdd9f6a30f6ce2c2472eafd382a0b9d
Evelynn LaCroix <evenlynn.h22@gmail.com>
2024-11-26 20:48:21 -0800

    WebPack and Rust INI x2

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const webpack = require("webpack");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = (env, args) => {
   const isProduction = args.mode === "production";
   return {
+    experiments: {
+      asyncWebAssembly: true,
+    },
     entry: "./index.js",
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: isProduction ? "[name].[contenthash].js" : "[name].[hash].js",
+      clean: true, // Automatically clean the output directory in Webpack 5+
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -18,10 +23,16 @@ module.exports = (env, args) => {
       new WasmPackPlugin({
         crateDirectory: path.resolve(__dirname, "."),
       }),
-      new webpack.ProvidePlugin({
-        TextDecoder: ["text-encoding", "TextDecoder"],
-        TextEncoder: ["text-encoding", "TextEncoder"],
-      }),
+      new CleanWebpackPlugin(), // Cleans up old files in the output directory
     ],
+    resolve: {
+      extensions: [".js", ".wasm"], // Automatically resolve .wasm modules
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all", // Improve caching by splitting shared modules
+      },
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map", // Improve debugging
   };
 };
